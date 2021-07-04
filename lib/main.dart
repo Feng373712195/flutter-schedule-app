3d3769fd5f52e203c5525b1e0612b554a11e694c
FENG373712195 <373712195@qq.com>
2021-07-04 23:52:04 +0800

    study fultter day12

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,109 @@
-import 'package:flutter/foundation.dart';
-import 'package:web_socket_channel/io.dart';
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flutter/material.dart';
 
-// good
-
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
+class AnimatedListSample extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    final title = 'WebSocket Demo';
-    return new MaterialApp(
-        title: title,
-        home: new MyHomePage(
-          title: title,
-          channel: new IOWebSocketChannel.connect('ws://echo.websocket.org'),
-        ));
+  _AnimatedListSampleState createState() => new _AnimatedListSampleState();
+}
+
+class _AnimatedListSampleState extends State<AnimatedListSample> {
+  final GlobalKey<AnimatedListState> _listKey =
+      new GlobalKey<AnimatedListState>();
+
+  ListModel<int> _list;
+  int _selectedItem;
+  int _nextItem;
+
+  void initState() {
+    super.initState();
+    _list = new ListModel<int>(
+      listKey: _listKey,
+      initialItems: <int>[0, 1, 2],
+      removedItemBuilder: _buildRemovedItem,
+    );
+    _nextItem = 3;
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  final String title;
-  final WebSocketChannel channel;
-
-  MyHomePage({Key key, @required this.title, @required this.channel})
-      : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _controller = new TextEditingController();
-
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            new Form(
-                child: new TextFormField(
-              controller: _controller,
-              decoration: new InputDecoration(labelText: 'Send a message'),
-            )),
-            new StreamBuilder(
-                stream: widget.channel.stream,
-                builder: (context, snapshot) {
-                  return new Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 24.0),
-                    child: new Text(snapshot.hasData ? '${snapshot.data}' : ''),
-                  );
-                })
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _sendMessage,
-        tooltip: 'Send message',
-        child: new Icon(Icons.send),
-      ),
+  Widget _buildItem(
+      BuildContext context, int index, Animation<double> animation) {
+    return new CardItem(
+      animation: animation,
+      item: _list[index],
+      selected: _selectedItem == _list[index],
+      onTap: () {
+        setState(() {
+          _selectedItem = _selectedItem == _list[index] ? null : _list[index];
+        });
+      },
     );
   }
 
-  void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      widget.channel.sink.add(_controller.text);
-    }
+  void _insert() {
+    final int index = _selectedItem == null ? _list.length : _list.indexOf();
+    _list.insert(index, _nextItem++);
   }
 
-  @override
-  void dispose() {
-    widget.channel.sink.close();
-    super.dispose();
+  void remove() {
+    if (_selectedItem != null) {
+      _list.removeAt(_list.indexOf(_selectedItem));
+      setState(() {
+        _selectedItem = null;
+      });
+    }
+  }
+}
+
+Widget _buildRemovedItem(
+    int item, BuildContext context, Animation<double> animation) {
+  return new CardItem(
+    animation: animation,
+    item: item,
+    selected: false,
+  );
+}
+
+class CardItem extends StatelessWidget {
+  const CardItem(
+      {Key key,
+      @required this.animation,
+      this.onTap,
+      @required this.item,
+      this.selected: false})
+      : assert(animation != null),
+        assert(item != null && item >= 0),
+        assert(selected != null),
+        super(key: key);
+
+  final Animation<double> animation;
+  final VoidCallback onTap;
+  final int item;
+  final bool selected;
+
+  Widget build(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.display1;
+    if (selected)
+      textStyle = textStyle.copyWith(color: Colors.lightGreenAccent[400]);
+    return new Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: new SizeTransition(
+        sizeFactor: animation,
+        child: new GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: onTap,
+          child: new SizedBox(
+            height: 128.0,
+            child: new Card(
+              color: Colors.primaries[item % Colors.primaries.length],
+              child: new Center(
+                child: new Text(
+                  'Item $item',
+                  style: textStyle,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
